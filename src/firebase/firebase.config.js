4b742ef96cd88a564b97b350033ce0977bb00922
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2024-02-20 09:19:30 +0100

    coffe-client-added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6HhG4omj2qJn83LVdxsc1hQn7q2I0A0o",
+  authDomain: "coffee-store-29b6f.firebaseapp.com",
+  projectId: "coffee-store-29b6f",
+  storageBucket: "coffee-store-29b6f.appspot.com",
+  messagingSenderId: "919334982871",
+  appId: "1:919334982871:web:93303ddd8d92bcd0a54c92",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
